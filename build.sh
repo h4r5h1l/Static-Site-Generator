8d20fe0a59acdf743f641e6ba2a74b1d69b7c5e6
h4r5h1l <h4r5h1l@gmail.com>
2025-12-19 10:20:52 -0500

    corrected basepath in buil.sh and built again

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Build using module invocation so package-relative imports resolve correctly
-python3 -m src.main "/https://github.com/h4r5h1l/Static-Site-Generator/"
+python3 -m src.main "/Static-Site-Generator/"
